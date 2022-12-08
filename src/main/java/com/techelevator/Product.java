f5d4808e8f090b84fb7d0f62476a47722efb2c38
Dez <desireemcinerney@Desirees-MacBook-Pro-3.local>
2022-12-08 10:33:21 -0800

    first update after meeting on 12/8

---
+++ src/main/java/com/techelevator/Product.java
@@ -0,0 +1,7 @@
+package com.techelevator;
+
+public class Product {
+    //selectProductFromList
+    //getListOfProducts
+    //updateProduct - use to update quantity
+}
---
+++ src/main/java/com/techelevator/PurchasingMenu.java
@@ -0,0 +1,13 @@
+package com.techelevator;
+
+import com.techelevator.view.Menu;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class PurchasingMenu extends Menu {
+
+    public PurchasingMenu(InputStream input, OutputStream output) {
+        super(input, output);
+    }
+}
