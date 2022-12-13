26c5aa251234a80ec690f6d9128852f4917a953f
abirami <abirami.arumugam@icloud.com>
2022-12-12 19:33:32 -0500

    finishTransaction

--- src/main/java/com/techelevator/PurchasingMenu.java
+++ src/main/java/com/techelevator/PurchasingMenu.java
@@ -12,4 +12,21 @@ public class PurchasingMenu extends Menu {
     }
 
 
+    public void finishTransaction(BigDecimal balance) {
+        double balance1;
+        int quarter;
+        int dime;
+        int nickel;
+        balance1 = (balance.doubleValue() * 100);
+        quarter = ((int) balance1 / 25);
+        balance1 = balance1 - (quarter * 25);
+        dime = ((int) balance1 / 10);
+        balance1 = balance1 - (dime* 10);
+        nickel = ((int) balance1 / 5);
+
+
+
+        System.out.println(
+                "Your change is " + quarter + " quarters, " + dime + " dimes and " + nickel + " nickels. ");
+    }
 }
