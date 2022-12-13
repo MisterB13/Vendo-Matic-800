1f3325d92febdb44310d51ed98b735713301baf6
Samuel <samuel.lindholm96@gmail.com>
2022-12-12 19:02:59 -0600

    Pushing for currency

--- src/main/java/com/techelevator/PurchasingMenu.java
+++ src/main/java/com/techelevator/PurchasingMenu.java
@@ -4,11 +4,42 @@ import com.techelevator.view.Menu;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PurchasingMenu extends Menu {
 
+    private static final String PURCHASING_MENU_FEED_MONEY = "Feed Money";
+
+    private static final String PURCHASING_MENU_SELECT_PRODUCT = "Select Product";
+
+    private static final String PURCHASING_MENU_FINISH_TRANSACTION = "Finish Transaction";
+    List<String> productList = new ArrayList<>();
+
+    private Menu menu;
     public PurchasingMenu(InputStream input, OutputStream output) {
         super(input, output);
+    }
+
+    public void run() {
+        String choice = (String) menu.getChoiceFromOptions();
+        while (true) {
+
+        }
+    }
+
+    public List<String> getProductList() {
+        return productList;
+    }
+
+    public void setProductList(List<String> productList) {
+        this.productList = getListOfProducts();
+    }
+
+    public void printProductList() {
+        System.out.println(productList);
+        System.out.println("Please enter item code from list above: ");
+
     }
 
 
--- src/main/java/com/techelevator/SelectProduct.java
+++ src/main/java/com/techelevator/SelectProduct.java
@@ -7,30 +7,52 @@ import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SelectProduct extends Menu, Product, implements Purchasable {
+public class SelectProduct extends Product implements Purchasable {
 
-    List<String> productList = new ArrayList<>();
+    List<String> productList = getListOfProducts;
     InputStream input;
 
     InputStream output;
 
-    public SelectProduct(InputStream input, OutputStream output, List<String> productList) {
+    private static final String CHIP_NOISE = "Crunch Crunch, Yum!";
+    private static final String CANDY_NOISE = "Munch Munch, Yum!";
+    private static final String DRINK_NOISE = "Glug Glug, Yum!";
+    private static final String GUM_NOISE = "Chew Chew, Yum!";
+
+    private String type = getType();
+
+    public SelectProduct(InputStream input, OutputStream output) {
         super(input, output);
-        this.productList = getListOfProducts();
+        Product product = new Product();
+        this.productList = productList;
     }
 
-     public void printProductList() {
-         System.out.println(productList);
-         System.out.println("Please enter item code from list above: ");
-     }
+    public void printProductList() {
+        System.out.println(productList);
+        System.out.println("Please enter item code from list above: ");
 
-     public void makeSelection() {
+    }
 
-     }
+    public void selectProduct(Object choice) {
+    }
 
-    public String makeNoise() {
-        if (input.contains()) {
+    public String dispenseProduct() {
+        String dispense = getProduct() + ": " + getPrice() + getRemainingBalance();
+        System.out.println(makeNoise(type));
+        return dispense;
+    }
 
+    public String makeNoise(String type) {
+        switch (type) {
+            case "Chip":
+                return CHIP_NOISE;
+            case "Candy":
+                return CANDY_NOISE;
+            case "Drink":
+                return DRINK_NOISE;
+            case "Gum":
+                return GUM_NOISE;
         }
+        return null;
     }
 }
