412b65bc1133dbcaaa8438abfee807dd59568816
abirami <abirami.arumugam@icloud.com>
2022-12-19 13:10:13 -0500

    feed money latest

--- src/main/java/com/techelevator/FeedMoney.java
+++ src/main/java/com/techelevator/FeedMoney.java
@@ -12,10 +12,8 @@ public class FeedMoney {
     Log writer = new Log();
 
 
-    public BigDecimal feedMoney() {
-        BigDecimal runningBalance = BigDecimal.valueOf(14.0);
-        runningBalance = runningBalance.add(new BigDecimal(1.0));
-        BigDecimal amountToAddBack = new BigDecimal(0);
+    public void feedMoney() {
+
         while (true) {
             Scanner scanner = new Scanner(System.in);
 
@@ -29,40 +27,43 @@ public class FeedMoney {
             System.out.println("2: 5 Dollars");
             System.out.println("3: 10 Dollars");
             System.out.println("4: 20 Dollars");
-            System.out.println("0: done");
+            System.out.println("0: return to previous menu");
             String choice = scanner.nextLine();
 
 
 
             if (choice.equals("1")) {
-                amountToAddBack = new BigDecimal(1.0);
-                runningBalance = runningBalance.add(new BigDecimal(1.0));
 
-                writer.writer(FEED_MONEY, new BigDecimal(1.00).setScale(2), runningBalance.add(runningBalance));
-                System.out.println("$1 is added, balance is now " + runningBalance);
+                Balance.add(1);
+
+                writer.writer(FEED_MONEY, new BigDecimal(1.00).setScale(2), Balance.getBalance());
+                System.out.println("$1 is added, balance is now " + Balance.getBalance());
             } else if (choice.equals("2")) {
-                amountToAddBack = new BigDecimal(2.00);
-                runningBalance = runningBalance.add(new BigDecimal(5.0));
-                writer.writer(FEED_MONEY, new BigDecimal(2.00).setScale(2), runningBalance.add(runningBalance));
-                System.out.println("$5 is added, balance is now " + runningBalance);
+
+                Balance.add(5);
+                writer.writer(FEED_MONEY, new BigDecimal(2.00).setScale(2), Balance.getBalance());
+                System.out.println("$5 is added, balance is now " + Balance.getBalance());
             } else if (choice.equals("3")) {
-                amountToAddBack = new BigDecimal(5.00);
-                runningBalance = runningBalance.add(new BigDecimal(10.0));
-                writer.writer(FEED_MONEY, new BigDecimal(5.00).setScale(2), runningBalance.add(amountToAddBack));
-                System.out.println("$10 is added, balance is now " + runningBalance);
+
+                Balance.add(10);
+                writer.writer(FEED_MONEY, new BigDecimal(5.00).setScale(2), Balance.getBalance());
+                System.out.println("$10 is added, balance is now " + Balance.getBalance());
             } else if (choice.equals("4")) {
-                amountToAddBack = new BigDecimal(10.00);
-                runningBalance = runningBalance.add(new BigDecimal(20.0));
-                writer.writer(FEED_MONEY, new BigDecimal(10.00).setScale(2), runningBalance.add(amountToAddBack));
-                System.out.println("$20 is added, balance is now " + runningBalance);
+
+                Balance.add(20);
+                writer.writer(FEED_MONEY, new BigDecimal(10.00).setScale(2), Balance.getBalance());
+                System.out.println("$20 is added, balance is now " + Balance.getBalance());
             } else if (choice.equals("0")) {
+                System.out.println("Your Balance is " + Balance.getBalance());
                 break;
+
             }
 
 
         }
 
-        return amountToAddBack;
+
+
     }
 
 
