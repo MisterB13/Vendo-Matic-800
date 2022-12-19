cdaf36dc9b52c209fbde7566b7fdf1b69ef95949
abirami <abirami.arumugam@icloud.com>
2022-12-19 11:52:32 -0500

    feed money working

--- src/main/java/com/techelevator/FeedMoney.java
+++ src/main/java/com/techelevator/FeedMoney.java
@@ -1,6 +1,8 @@
 package com.techelevator;
 
 import com.techelevator.data.Log;
+import com.techelevator.data.Repo;
+import com.techelevator.view.Menu;
 
 import java.math.BigDecimal;
 import java.util.Scanner;
@@ -9,42 +11,60 @@ public class FeedMoney {
     private static final String FEED_MONEY = "FEED_MONEY";
     Log writer = new Log();
 
-    public BigDecimal feedMoney(String choice, BigDecimal runningBalance) {
-        Scanner scanner = new Scanner(System.in);
 
-        String feedMoney = scanner.nextLine();
-        System.out.println("Feed Money");
-
+    public BigDecimal feedMoney() {
+        BigDecimal runningBalance = BigDecimal.valueOf(14.0);
+        runningBalance = runningBalance.add(new BigDecimal(1.0));
         BigDecimal amountToAddBack = new BigDecimal(0);
-
-        System.out.println("1: 1 Dollar");
-        System.out.println("2: 5 Dollars");
-        System.out.println("3: 10 Dollars");
-        System.out.println("4: 20 Dollars");
-        System.out.println("0: done");
-        boolean feedMoneyLoopContinues = true;
-        while(feedMoneyLoopContinues){
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
 
 
-        if (choice.equals("1")) {
-            amountToAddBack = new BigDecimal(1.00);
-            writer.writer(FEED_MONEY, new BigDecimal(1.00).setScale(2), runningBalance.add(amountToAddBack) );
-        } else if (choice.equals("2")) {
-            amountToAddBack = new BigDecimal(2.00);
-            writer.writer(FEED_MONEY, new BigDecimal(2.00).setScale(2), runningBalance.add(amountToAddBack));
-        } else if (choice.equals("3")) {
-            amountToAddBack = new BigDecimal(5.00);
-            writer.writer(FEED_MONEY, new BigDecimal(5.00).setScale(2), runningBalance.add(amountToAddBack));
-        } else if (choice.equals("4")) {
-            amountToAddBack = new BigDecimal(10.00);
-            writer.writer(FEED_MONEY, new BigDecimal(10.00).setScale(2), runningBalance.add(amountToAddBack));
+            System.out.println("Feed Money");
+
+
+
+
+            System.out.println("1: 1 Dollar");
+            System.out.println("2: 5 Dollars");
+            System.out.println("3: 10 Dollars");
+            System.out.println("4: 20 Dollars");
+            System.out.println("0: done");
+            String choice = scanner.nextLine();
+
+
+
+            if (choice.equals("1")) {
+                amountToAddBack = new BigDecimal(1.0);
+                runningBalance = runningBalance.add(new BigDecimal(1.0));
+
+                writer.writer(FEED_MONEY, new BigDecimal(1.00).setScale(2), runningBalance.add(runningBalance));
+                System.out.println("$1 is added, balance is now " + runningBalance);
+            } else if (choice.equals("2")) {
+                amountToAddBack = new BigDecimal(2.00);
+                runningBalance = runningBalance.add(new BigDecimal(5.0));
+                writer.writer(FEED_MONEY, new BigDecimal(2.00).setScale(2), runningBalance.add(runningBalance));
+                System.out.println("$5 is added, balance is now " + runningBalance);
+            } else if (choice.equals("3")) {
+                amountToAddBack = new BigDecimal(5.00);
+                runningBalance = runningBalance.add(new BigDecimal(10.0));
+                writer.writer(FEED_MONEY, new BigDecimal(5.00).setScale(2), runningBalance.add(amountToAddBack));
+                System.out.println("$10 is added, balance is now " + runningBalance);
+            } else if (choice.equals("4")) {
+                amountToAddBack = new BigDecimal(10.00);
+                runningBalance = runningBalance.add(new BigDecimal(20.0));
+                writer.writer(FEED_MONEY, new BigDecimal(10.00).setScale(2), runningBalance.add(amountToAddBack));
+                System.out.println("$20 is added, balance is now " + runningBalance);
+            } else if (choice.equals("0")) {
+                break;
+            }
+
+
         }
+
         return amountToAddBack;
-
-        if(choice.equals(0)){
-            feedMoneyLoopContinues = false;
-        }
     }
 
 
-}
+
+    }
